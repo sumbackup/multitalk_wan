a2e926024c976ea0e435277dde26fe37ad6e2127
sumbackup <sumanthdbackup@gmail.com>
2025-09-19 01:44:30 +0530

    Update workflow.py

--- workflow.py
+++ workflow.py
@@ -108,6 +108,7 @@ def import_custom_nodes() -> None:
     server_instance = server.PromptServer(loop)
     execution.PromptQueue(server_instance)
 
+    # Initializing custom nodes
     try:
         # Try async version first
         loop.run_until_complete(init_extra_nodes())
@@ -116,17 +117,16 @@ def import_custom_nodes() -> None:
         init_extra_nodes()
 
 
-from nodes import LoadImage, NODE_CLASS_MAPPINGS, CLIPVisionLoader
+from nodes import NODE_CLASS_MAPPINGS, CLIPVisionLoader
 
 
-class WanVideoMultiTalk:
+class WanVideoInfiniteTalk:
     def __init__(self):
         import_custom_nodes()
         
         # Initialize all model loaders and nodes
         self.multitalkmodelloader = NODE_CLASS_MAPPINGS["MultiTalkModelLoader"]()
         self.wanvideovaeloader = NODE_CLASS_MAPPINGS["WanVideoVAELoader"]()
-        self.loadimage = LoadImage()
         self.wanvideoblockswap = NODE_CLASS_MAPPINGS["WanVideoBlockSwap"]()
         self.loadwanvideot5textencoder = NODE_CLASS_MAPPINGS["LoadWanVideoT5TextEncoder"]()
         self.downloadandloadwav2vecmodel = NODE_CLASS_MAPPINGS["DownloadAndLoadWav2VecModel"]()
@@ -134,26 +134,24 @@ class WanVideoMultiTalk:
         self.wanvideotorchcompilesettings = NODE_CLASS_MAPPINGS["WanVideoTorchCompileSettings"]()
         self.vhs_loadaudioupload = NODE_CLASS_MAPPINGS["VHS_LoadAudioUpload"]()
         self.wanvideoloraselect = NODE_CLASS_MAPPINGS["WanVideoLoraSelect"]()
+        self.loadandresizeimage = NODE_CLASS_MAPPINGS["LoadAndResizeImage"]()
+        self.easy_float = NODE_CLASS_MAPPINGS["easy float"]()
         self.wanvideomodelloader = NODE_CLASS_MAPPINGS["WanVideoModelLoader"]()
         self.vram_debug = NODE_CLASS_MAPPINGS["VRAM_Debug"]()
-        self.get_image_size = NODE_CLASS_MAPPINGS["Get Image Size"]()
-        self.imageresizekjv2 = NODE_CLASS_MAPPINGS["ImageResizeKJv2"]()
-        self.easy_mathfloat = NODE_CLASS_MAPPINGS["easy mathFloat"]()
-        self.cr_float_to_integer = NODE_CLASS_MAPPINGS["CR Float To Integer"]()
         self.wanvideoclipvisionencode = NODE_CLASS_MAPPINGS["WanVideoClipVisionEncode"]()
-        self.wanvideoimagetovideoencode = NODE_CLASS_MAPPINGS["WanVideoImageToVideoEncode"]()
+        self.wanvideoimagetovideomultitalk = NODE_CLASS_MAPPINGS["WanVideoImageToVideoMultiTalk"]()
         self.wanvideotextencode = NODE_CLASS_MAPPINGS["WanVideoTextEncode"]()
-        self.audiocrop = NODE_CLASS_MAPPINGS["AudioCrop"]()
-        self.audioseparation = NODE_CLASS_MAPPINGS["AudioSeparation"]()
         self.layermask_segmentanythingultra_v2 = NODE_CLASS_MAPPINGS["LayerMask: SegmentAnythingUltra V2"]()
         self.multitalkwav2vecembeds = NODE_CLASS_MAPPINGS["MultiTalkWav2VecEmbeds"]()
         self.wanvideosampler = NODE_CLASS_MAPPINGS["WanVideoSampler"]()
         self.wanvideodecode = NODE_CLASS_MAPPINGS["WanVideoDecode"]()
         self.vhs_videocombine = NODE_CLASS_MAPPINGS["VHS_VideoCombine"]()
+        self.audio_duration_mtb = NODE_CLASS_MAPPINGS["Audio Duration (mtb)"]()
+        self.mathexpressionpysssss = NODE_CLASS_MAPPINGS["MathExpression|pysssss"]()
 
         # Load models
         self.multitalkmodelloader_120 = self.multitalkmodelloader.loadmodel(
-            model="WanVideo_2_1_Multitalk_14B_fp8_e4m3fn.safetensors"
+            model="Wan2_1-InfiniTetalk-Single_fp16.safetensors"
         )
 
         self.wanvideovaeloader_129 = self.wanvideovaeloader.loadmodel(
@@ -202,26 +200,26 @@ class WanVideoMultiTalk:
             strength=1,
             low_mem_load=False,
             merge_loras=True,
-            unique_id=11824802763977056194,
+            unique_id=12486806784525555066,
         )
 
         # Additional LoRA selections
         self.wanvideoloraselect_211 = self.wanvideoloraselect.getlorapath(
-            lora="Wan14Bi2vFusioniX_pure_fp16.safetensors",
+            lora="Wan2.1_I2V_14B_FusionX_LoRA.safetensors",
             strength=1,
             low_mem_load=False,
             merge_loras=True,
             prev_lora=get_value_at_index(self.wanvideoloraselect_238, 0),
-            unique_id=10030556395511193686,
+            unique_id=11251795944855334678,
         )
 
         self.wanvideoloraselect_138 = self.wanvideoloraselect.getlorapath(
-            lora="Lightx2v/lightx2v_T2V_14B_cfg_step_distill_v2_lora_rank256_bf16.safetensors",
-            strength=0.8000000000000002,
+            lora="Lightx2v/lightx2v_I2V_14B_480p_cfg_step_distill_rank64_bf16.safetensors",
+            strength=0.8,
             low_mem_load=False,
             merge_loras=True,
             prev_lora=get_value_at_index(self.wanvideoloraselect_211, 0),
-            unique_id=6129929711643136840,
+            unique_id=6793129974915389832,
         )
 
     @torch.inference_mode()
@@ -229,15 +227,36 @@ class WanVideoMultiTalk:
         # Get parameters from kwargs
         image_path = kwargs.get("image", "input_image.png")
         audio_path = kwargs.get("audio", "input_audio.mp3")
-        positive_prompt = kwargs.get("positive_prompt", "A woman speakinng passionately about a face cream that she loves")
+        positive_prompt = kwargs.get("positive_prompt", "A woman passionately talking while holding a book a hand")
         negative_prompt = kwargs.get("negative_prompt", "bright tones, overexposed, static, blurred details, subtitles, style, works, paintings, images, static, overall gray, worst quality, low quality, JPEG compression residue, ugly, incomplete, extra fingers, poorly drawn hands, poorly drawn faces, deformed, disfigured, misshapen limbs, fused fingers, still picture, messy background, three legs, many people in the background, walking backwards")
-        steps = kwargs.get("steps", 4)
-        cfg = kwargs.get("cfg", 1.0)
-        shift = kwargs.get("shift", 11.0)
         seed = kwargs.get("seed", random.randint(1, 2**64))
-        lora_path = kwargs.get("lora_path", None)
-        filename_prefix = kwargs.get("filename_prefix", "WanVideo2_1_multitalk")
         fps = kwargs.get("fps", 25)
+        filename_prefix = kwargs.get("filename_prefix", "WanVideo2_1_multitalk")
+        lora_path = kwargs.get("lora_path", None)
+        # Fixed parameters optimized for this model
+        steps = 4
+        cfg = 1
+        shift = 11
+
+        # Load input image and audio
+        loadandresizeimage_240 = self.loadandresizeimage.load_image(
+            image=image_path,
+            resize=True,
+            width=720,
+            height=1280,
+            repeat=1,
+            keep_proportion=True,
+            divisible_by=16,
+            mask_channel="alpha",
+            background_color="",
+        )
+
+        vhs_loadaudioupload_212 = self.vhs_loadaudioupload.load_audio(
+            audio=audio_path, start_time=0, duration=0
+        )
+
+        easy_float_243 = self.easy_float.execute(value=fps)
+
         # Model loading
         if lora_path is not None:
             final_lora = self.wanvideoloraselect.getlorapath(
@@ -252,10 +271,10 @@ class WanVideoMultiTalk:
         else:
             print('no custom lora to load')
             final_lora = self.wanvideoloraselect_138
-
+        # Model loading
         wanvideomodelloader_122 = self.wanvideomodelloader.loadmodel(
             model="wan2.1_i2v_720p_14B_bf16.safetensors",
-            base_precision="fp16_fast",
+            base_precision="bf16",
             quantization="fp8_e4m3fn",
             load_device="offload_device",
             attention_mode="sdpa",
@@ -265,45 +284,11 @@ class WanVideoMultiTalk:
             multitalk_model=get_value_at_index(self.multitalkmodelloader_120, 0),
         )
 
-        # VRAM debug
         vram_debug_206 = self.vram_debug.VRAMdebug(
             empty_cache=True,
             gc_collect=True,
-            unload_all_models=False,
+            unload_all_models=True,
             any_input=get_value_at_index(wanvideomodelloader_122, 0),
-        )
-        # Load input image and audio
-        loadimage_133 = self.loadimage.load_image(image=image_path)
-        vhs_loadaudioupload_212 = self.vhs_loadaudioupload.load_audio(
-            audio=audio_path, start_time=0, duration=0
-        )
-
-
-        get_image_size_239 = self.get_image_size.get_size(
-            image=get_value_at_index(loadimage_133, 0)
-        )
-
-        imageresizekjv2_225 = self.imageresizekjv2.resize(
-            width=get_value_at_index(get_image_size_239, 0),
-            height=get_value_at_index(get_image_size_239, 1),
-            upscale_method="nearest-exact",
-            keep_proportion="resize",
-            pad_color="0, 0, 0",
-            crop_position="center",
-            divisible_by=16,
-            device="cpu",
-            image=get_value_at_index(loadimage_133, 0),
-            unique_id=5063178432147508168,
-        )
-
-        easy_mathfloat_214 = self.easy_mathfloat.float_math_operation(
-            a=get_value_at_index(vhs_loadaudioupload_212, 1),
-            b=16.000000000000004,
-            operation="multiply",
-        )
-
-        cr_float_to_integer_217 = self.cr_float_to_integer.convert(
-            _float=get_value_at_index(easy_mathfloat_214, 0)
         )
 
         wanvideoclipvisionencode_193 = self.wanvideoclipvisionencode.process(
@@ -313,24 +298,24 @@ class WanVideoMultiTalk:
             combine_embeds="average",
             force_offload=True,
             tiles=0,
-            ratio=0.5000000000000001,
+            ratio=0.5,
             clip_vision=get_value_at_index(self.clipvisionloader_173, 0),
-            image_1=get_value_at_index(imageresizekjv2_225, 0),
+            image_1=get_value_at_index(loadandresizeimage_240, 0),
         )
 
-        wanvideoimagetovideoencode_207 = self.wanvideoimagetovideoencode.process(
-            width=get_value_at_index(imageresizekjv2_225, 1),
-            height=get_value_at_index(imageresizekjv2_225, 2),
-            num_frames=get_value_at_index(cr_float_to_integer_217, 0),
-            noise_aug_strength=0,
-            start_latent_strength=1,
-            end_latent_strength=1,
+        wanvideoimagetovideomultitalk_267 = self.wanvideoimagetovideomultitalk.process(
+            width=get_value_at_index(loadandresizeimage_240, 2),
+            height=get_value_at_index(loadandresizeimage_240, 3),
+            frame_window_size=81,
+            motion_frame=9,
             force_offload=True,
-            fun_or_fl2v_model=False,
+            colormatch="disabled",
             tiled_vae=False,
+            mode="infinitetalk",
+            output_path="",
             vae=get_value_at_index(self.wanvideovaeloader_129, 0),
+            start_image=get_value_at_index(loadandresizeimage_240, 0),
             clip_embeds=get_value_at_index(wanvideoclipvisionencode_193, 0),
-            start_image=get_value_at_index(imageresizekjv2_225, 0),
         )
 
         wanvideotextencode_135 = self.wanvideotextencode.process(
@@ -340,19 +325,6 @@ class WanVideoMultiTalk:
             use_disk_cache=False,
             device="gpu",
             t5=get_value_at_index(self.loadwanvideot5textencoder_136, 0),
-        )
-
-        audiocrop_159 = self.audiocrop.main(
-            start_time="0:00",
-            end_time="2:00",
-            audio=get_value_at_index(vhs_loadaudioupload_212, 0),
-        )
-
-        audioseparation_170 = self.audioseparation.main(
-            chunk_fade_shape="linear",
-            chunk_length=10,
-            chunk_overlap=0.1,
-            audio=get_value_at_index(audiocrop_159, 0),
         )
 
         layermask_segmentanythingultra_v2_200 = (
@@ -370,19 +342,19 @@ class WanVideoMultiTalk:
                 device="cuda",
                 max_megapixels=2,
                 cache_model=False,
-                image=get_value_at_index(imageresizekjv2_225, 0),
+                image=get_value_at_index(loadandresizeimage_240, 0),
             )
         )
 
         multitalkwav2vecembeds_194 = self.multitalkwav2vecembeds.process(
             normalize_loudness=True,
-            num_frames=get_value_at_index(cr_float_to_integer_217, 0),
-            fps=fps,
+            num_frames=3000,
+            fps=get_value_at_index(easy_float_243, 0),
             audio_scale=1,
             audio_cfg_scale=1,
             multi_audio_type="para",
             wav2vec_model=get_value_at_index(self.downloadandloadwav2vecmodel_137, 0),
-            audio_1=get_value_at_index(audioseparation_170, 3),
+            audio_1=get_value_at_index(vhs_loadaudioupload_212, 0),
             ref_target_masks=get_value_at_index(
                 layermask_segmentanythingultra_v2_200, 1
             ),
@@ -403,7 +375,7 @@ class WanVideoMultiTalk:
             end_step=-1,
             add_noise_to_samples=False,
             model=get_value_at_index(vram_debug_206, 0),
-            image_embeds=get_value_at_index(wanvideoimagetovideoencode_207, 0),
+            image_embeds=get_value_at_index(wanvideoimagetovideomultitalk_267, 0),
             text_embeds=get_value_at_index(wanvideotextencode_135, 0),
             multitalk_embeds=get_value_at_index(multitalkwav2vecembeds_194, 0),
         )
@@ -420,21 +392,20 @@ class WanVideoMultiTalk:
         )
 
         vhs_videocombine_131 = self.vhs_videocombine.combine_video(
-            frame_rate=fps,
+            frame_rate=get_value_at_index(easy_float_243, 0),
             loop_count=0,
             filename_prefix=filename_prefix,
             format="video/h264-mp4",
             pix_fmt="yuv420p",
             crf=19,
             save_metadata=True,
-            trim_to_audio=False,
+            trim_to_audio=True,
             pingpong=False,
             save_output=True,
             images=get_value_at_index(wanvideodecode_130, 0),
-            audio=get_value_at_index(audiocrop_159, 0),
-            unique_id=10543100069109904756,
+            audio=get_value_at_index(vhs_loadaudioupload_212, 0),
+            unique_id=7884768345825743247,
         )
-
         # Cleanup: Free GPU memory
         del wanvideomodelloader_122
         del vram_debug_206
@@ -444,15 +415,15 @@ class WanVideoMultiTalk:
 
 
 if __name__ == "__main__":
-    model = WanVideoMultiTalk()
+    model = WanVideoInfiniteTalk()
     result = model(
-        positive_prompt="A woman speakinng passionately about a face cream that she loves",
+        positive_prompt="A woman passionately talking while holding a book a hand",
         negative_prompt="bright tones, overexposed, static, blurred details, subtitles, style, works, paintings, images, static, overall gray, worst quality, low quality, JPEG compression residue, ugly, incomplete, extra fingers, poorly drawn hands, poorly drawn faces, deformed, disfigured, misshapen limbs, fused fingers, still picture, messy background, three legs, many people in the background, walking backwards",
         steps=4,
-        cfg=1.0000000000000002,
-        shift=11.000000000000002,
+        cfg=1,
+        shift=11,
         seed=random.randint(1, 2**64),
-        num_iterations=10,
+        fps=25,
         filename_prefix="WanVideo2_1_multitalk"
     )
     print('result:', result)
