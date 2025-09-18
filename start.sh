e6a1201b3292a0d17127a23003d91402471ad43c
sumbackup <sumanthdbackup@gmail.com>
2025-09-19 01:45:24 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,14 @@
+#!/bin/bash
+
 cd /app
+
+if [[ $CLONE ]]
+then
+    echo "Cloning rp_handler.py and workflow.py"
+    rm -rf rp_handler.py workflow.py
+    curl https://raw.githubusercontent.com/sumbackup/multitalk_wan/refs/heads/main/rp_handler.py > rp_handler.py
+    curl https://raw.githubusercontent.com/sumbackup/multitalk_wan/refs/heads/main/workflow.py > workflow.py
+fi
+
+echo "Starting rp_handler.py"
 python rp_handler.py
